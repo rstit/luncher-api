356319581a9e03e11f2ba757a2adf051d3a30797
Krzysztof Świątek <krzysztof.swiatek@stxnext.pl>
2018-08-21 10:52:00 +0200

    Use marshmallow to create meals object

--- luncher/meals/views.py
+++ luncher/meals/views.py
@@ -16,11 +16,12 @@ from utils.status import HTTPStatus
 
 
 class MealListView(MethodView):
-    def __init__(self, dao=None, serializer=None):
+    def __init__(self, dao=None, serializer=None, meal_serializer=None):
         self.dao = dao or MealDao()
         self.mealtype_dao = BaseDao(MealType)
         self.venue_dao = VenueDao()
         self.serializer = serializer or MealSchema(many=True)
+        self.meal_serializer = meal_serializer or MealSchema()
 
     def get(self):
         data, _ = self.serializer.dump(
@@ -30,18 +31,10 @@ class MealListView(MethodView):
 
     @login_required
     def post(self):
-        meal_name = request.json.get('name')
-        meal_price = request.json.get('price')
-        meal_type = request.json.get('meal_type')
-        venue = request.json.get('venue')
-        if not meal_price or not meal_name:
-            return '', HTTPStatus.HTTP_400_BAD_REQUEST
+        validated_data, errors = self.meal_serializer.loads(
+            dumps(request.json))
+        if errors:
+            return errors, HTTPStatus.HTTP_400_BAD_REQUEST
 
-        meal_kw = {}
-        if meal_type:
-            meal_kw['type_id'] = self.mealtype_dao.get(value=meal_type).id
-        if venue:
-            meal_kw['venue_id'] = self.venue_dao.get(name=venue).id
-
-        new_meal = self.dao.create(name=meal_name, price=meal_price, **meal_kw)
+        new_meal = self.dao.create(validated_data)
         return dumps({'new_meal_id': new_meal.id}), HTTPStatus.HTTP_201_CREATED
